778f4518603240547b5243533ca2e6c454cba13c
emersonat <emersonat@ig.com.br>
2018-10-21 21:25:51 -0300

    alteracoes

--- funcoes_nomeadas.ex
+++ funcoes_nomeadas.ex
@@ -1,13 +1,18 @@
 #exemplo de modulo
-defmodule Funcoes_nomeadas do
-    
+defmodule FuncoesNomeadas do
+
     def multiplique(a,b \\ 2) do
         soma(10,a * b)
     end
 
    defp soma(num1,num2) do
        num1 + num2
-       
+
    end
+
+   def saudacao(gretting, name), do: (
+       IO.puts gretting
+       IO.puts "seu nome #{name}"
+   )
 
 end
